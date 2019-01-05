2f27308321582b6af60e7f17a216948a9c8c690b
l1x3 <lixe@posteo.de>
2019-01-05 09:30:26 +0100

    change clienttype to enum

--- src/main/java/de/thi/iis/Bestellung_per_Drohne/Client.java
+++ src/main/java/de/thi/iis/Bestellung_per_Drohne/Client.java
@@ -5,12 +5,11 @@ package de.thi.iis.Bestellung_per_Drohne;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -28,7 +27,50 @@ public class Client {
 	private String street;
 	@Column(name = "place")
 	private String place;
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "clienttype")
+	@Enumerated(EnumType.ORDINAL)
 	private ClientType clienttype;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public ClientType getClienttype() {
+		return clienttype;
+	}
+
+	public void setClienttype(ClientType clienttype) {
+		this.clienttype = clienttype;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
--- src/main/java/de/thi/iis/Bestellung_per_Drohne/ClientType.java
+++ src/main/java/de/thi/iis/Bestellung_per_Drohne/ClientType.java
@@ -3,20 +3,6 @@
 // #######################################
 package de.thi.iis.Bestellung_per_Drohne;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "clienttype")
-public class ClientType {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private int id;
-	@Column(name = "name")
-	private String name;
+public enum ClientType {
+	DONOTUSE, NORMAL, PREMIUM, SUPER;
 }
