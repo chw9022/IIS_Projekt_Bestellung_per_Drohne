ef45fba2cb9aa40572b3e6bf846306e4179f6267
l1x3 <lixe@posteo.de>
2019-01-05 20:45:43 +0100

    add clients class to manipulate client entities

---
+++ src/main/java/de/thi/iis/Bestellung_per_Drohne/Clients.java
@@ -0,0 +1,33 @@
+// #######################################
+// Author: Felix Ziegner
+// #######################################
+package de.thi.iis.Bestellung_per_Drohne;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Clients {
+	EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("GroceryShopPersistence");
+	EntityManager entityManager = emFactory.createEntityManager();
+
+	public void addClient(Client client) {
+		entityManager.persist(client);
+	}
+
+	public void deleteClient(Client client) {
+		entityManager.remove(client);
+	}
+
+	public Client findById(int clientId) {
+		  return entityManager.find(Client.class, clientId);
+	}
+
+	public List<Client> getClients() {
+		Query query = entityManager.createQuery("SELECT c from Client as c");
+		return query.getResultList();
+	}
+}
