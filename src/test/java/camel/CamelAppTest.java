fde69a96ec86e7ca156dbd56005d3c64594375e6
l1x3 <lixe@posteo.de>
2019-01-11 08:17:11 +0100

    adding test for CamelApp

---
+++ src/test/java/camel/CamelAppTest.java
@@ -0,0 +1,44 @@
+package camel;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import camel.routes.OrderArchiveRouteBuilder;
+
+public class CamelAppTest {
+
+	static {
+		LogFactory.useSlf4jLogging(); // MyBatis
+	}
+
+	@Before
+	public void setup() throws Exception {
+
+	}
+
+	@Test
+	public void runCamelApp() throws Exception {
+		CamelApp camelApp = new CamelApp();
+		assertFalse(camelApp.isStarted());
+		camelApp.start();
+		assertTrue(camelApp.isStarted());
+		camelApp.stop();
+		assertFalse(camelApp.isStarted());
+	}
+
+	@Test
+	public void checkContainsOrderArchiveRouteBuilder() throws Exception {
+		CamelApp camelApp = new CamelApp();
+		assertTrue(listContainsObjectWithClass(camelApp.getRouteBuilders(), OrderArchiveRouteBuilder.class));
+	}
+
+	private boolean listContainsObjectWithClass(List<?> objectList, Class objectClass) {
+		return objectList.stream().anyMatch(item -> item.getClass() == objectClass);
+	}
+}
