e5449e609b7b620e10f4d055bb7013ba5500c435
l1x3 <lixe@posteo.de>
2019-01-12 01:45:11 +0100

    process orders instead of articles, use the right formats for mashalling

--- src/main/java/camel/routes/OrderArchiveRouteBuilder.java
+++ src/main/java/camel/routes/OrderArchiveRouteBuilder.java
@@ -10,25 +10,24 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
 
-import jpa.entities.Article;
+import jpa.entities.Order;
 import processengine.delegate.ArchiveOrder;
 
 public class OrderArchiveRouteBuilder extends RouteBuilder {
-	private static String directory_archive = "file:/home/lars/Downloads";
-	private static String external_archive = "jms:queue:order-archive-external";
 
 	@Override
 	public void configure() throws Exception {
 		Endpoint source = endpoint("jms:queue:" + ArchiveOrder.QUEUE_ORDER_ARCHIVE);
-		Endpoint destination1 = endpoint(directory_archive);
-		Endpoint destination2 = endpoint(external_archive);
+		Endpoint destination1 = endpoint("file:/home/lars/Downloads");
+		Endpoint destination2 = endpoint("jms:queue:order-archive-external");
 
-		JAXBContext jaxbContext = JAXBContext.newInstance(Article.class);
+		JAXBContext jaxbContext = JAXBContext.newInstance(Order.class);
 		JaxbDataFormat jaxbDataFormat = new JaxbDataFormat(jaxbContext);
-
 		from(source) // from XML source
 				.unmarshal(jaxbDataFormat) // XML to POJO
-				.marshal().json(JsonLibrary.Jackson, Article.class) // POJO to JSON
-				.to(destination1).to(destination2);
+				.marshal(jaxbDataFormat) // POJO to XML
+				.to(destination1) // to filesystem
+				.marshal().json(JsonLibrary.Jackson, Order.class) // POJO to JSON
+				.to(destination2); // to queue
 	}
 }
