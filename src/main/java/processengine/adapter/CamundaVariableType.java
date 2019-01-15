a412b0ff00cddd744bc7ee1b1e06950ac5822855
l1x3 <lixe@posteo.de>
2019-01-15 01:18:36 +0100

    create adapter to send messages to camunda rest api

---
+++ src/main/java/processengine/adapter/CamundaVariableType.java
@@ -0,0 +1,11 @@
+// #######################################
+// Author: Felix Ziegner
+// #######################################
+package processengine.adapter;
+
+public final class CamundaVariableType {
+	public static final String STRING = "String";
+	public static final String OBJECT = "Object";
+	public static final String INTEGER = "Integer";
+	public static final String DOUBLE = "Double";
+}
