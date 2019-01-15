ba999caa56ea3d3abf6ecb19317e92620d5485de
l1x3 <lixe@posteo.de>
2019-01-16 00:33:46 +0100

    send drone startet message

---
+++ drone-order-process/src/main/java/iis/project/processengine/delegate/DroneStarted.java
@@ -0,0 +1,31 @@
+package iis.project.processengine.delegate;
+
+import java.io.StringWriter;
+import java.util.Calendar;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import iis.project.jms.JMSManager;
+import message.DroneStartedMessage;
+
+public class DroneStarted implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		JMSManager m = new JMSManager();
+		Integer droneId = (Integer) execution.getVariable("droneId");
+		Calendar timeOfArrival = Calendar.getInstance();
+		timeOfArrival.add(Calendar.MINUTE, 5);
+		DroneStartedMessage message = new DroneStartedMessage();
+		message.setDroneId(droneId);
+		JAXBContext jaxbContext = JAXBContext.newInstance(DroneStartedMessage.class);
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		StringWriter stringWriter = new StringWriter();
+		marshaller.marshal(message, stringWriter);
+		m.sendMessage(stringWriter.toString(), "drone-started");
+	}
+}
