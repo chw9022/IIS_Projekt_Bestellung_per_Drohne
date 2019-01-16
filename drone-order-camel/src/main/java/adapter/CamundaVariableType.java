e3e33fd3f9946f2e3d28e3fa14c982d3d5e39095
l1x3 <lixe@posteo.de>
2019-01-16 19:37:58 +0100

    add long

--- drone-order-camel/src/main/java/adapter/CamundaVariableType.java
+++ drone-order-camel/src/main/java/adapter/CamundaVariableType.java
@@ -8,4 +8,5 @@ public final class CamundaVariableType {
 	public static final String OBJECT = "Object";
 	public static final String INTEGER = "Integer";
 	public static final String DOUBLE = "Double";
+	public static final String LONG = "Long";
 }
