88df6e357948496d24029d87556e36cc0d9826ec
Christian Wittmann <chw9022@thi.de>
2019-01-18 16:45:39 +0100

    Added Camel-Route: drone-landed

--- drone-order-camel/src/main/java/CamelApp.java
+++ drone-order-camel/src/main/java/CamelApp.java
@@ -4,11 +4,14 @@
 // #######################################
 
 import javax.jms.ConnectionFactory;
+import javax.sql.DataSource;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.main.Main;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+import routes.DroneLandedRouteBuilder;
 import routes.MessageToCamundaRouteBuilder;
 import routes.OrderArchiveRouteBuilder;
 
@@ -28,10 +31,21 @@ public class CamelApp extends Main {
 	private void initialize() {
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://0.0.0.0:61616");
 		this.bind("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+		this.bind("groceryshopDataSource", getGroceryshopDataSource());
 	}
 
 	private void addRouteBuilders() {
 		this.addRouteBuilder(new OrderArchiveRouteBuilder());
 		this.addRouteBuilder(new MessageToCamundaRouteBuilder());
+		this.addRouteBuilder(new DroneLandedRouteBuilder());
+	}
+	
+	private DataSource getGroceryshopDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUsername("root");
+        ds.setPassword("master42");
+        ds.setUrl("jdbc:mysql://localhost:3306/groceryshop");
+        return ds;
 	}
 }
