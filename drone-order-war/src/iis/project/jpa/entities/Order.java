86437af2709c9cf350dca3eac199cbfced1725d2
Christian Wittmann <chw9022@thi.de>
2019-01-18 10:48:58 +0100

    Used droneId of order instead of creating a new Camunda-Variable

--- drone-order-war/src/iis/project/jpa/entities/Order.java
+++ drone-order-war/src/iis/project/jpa/entities/Order.java
@@ -41,9 +41,8 @@ public class Order implements Serializable {
 	@JoinColumn(name = "clientId")
 	private Client client;
 	
-	@ManyToOne(fetch = FetchType.LAZY ,cascade = {CascadeType.PERSIST})
-	@JoinColumn(name = "droneId")
-	private Drone drone;
+	@Column(name = "droneId")
+	private int droneId;
 	
 	@Column(name = "orderdate")
 	private Date orderdate;
@@ -75,12 +74,12 @@ public class Order implements Serializable {
 	}
 
 	@XmlElement
-	public Drone getDrone() {
-        return drone;
+    public int getDroneId() {
+        return droneId;
     }
 
-    public void setDrone(Drone drone) {
-        this.drone = drone;
+    public void setDroneId(int droneId) {
+        this.droneId = droneId;
     }
 
     @XmlElement
